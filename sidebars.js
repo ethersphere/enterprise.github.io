2f3f377f55071cabfe6d89b8b8fed4ab90f6e52d
Crt Ahlin <crtah@pop-os.localdomain>
2020-10-20 09:27:13 +0200

    merge some sidebar categories

--- sidebars.js
+++ sidebars.js
@@ -1,12 +1,12 @@
 module.exports = {
   someSidebar: {
-    'Swarm for Enterprise': ['toc', 'problems', 'intro', 'swarm',], 
+    'Swarm for Enterprise': ['toc', 'problems', 'intro', 'swarm',],
     'Data economy':['data-economy', 'catalyst', 'ethereum', 'new-paradigm', 'data-spaces', 'recommendations', ],
     Privacy: ['privacy', 'swarm-privacy', ],
-    'GDPR (Draft)': ['gdpr', 'consents',],
+    'Regulation': ['gdpr', 'consents',],
     Examples: ['fairos', 'fairdrive', 'personium', 'fairdrop',],
     References: ['references',],
-    Acknowledgements: ['acknowledgements',],
-    'How to contribute': ['contribute',],
+    Contribution: ['acknowledgements', 'contribute',],
   },
+
 };
