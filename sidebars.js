f094c81122b6450c2bfc69e8caeb00c4d55fb729
Crt Ahlin <crtah@pop-os.localdomain>
2020-10-20 10:19:29 +0200

    remove some content from sidebar

--- sidebars.js
+++ sidebars.js
@@ -1,12 +1,15 @@
 module.exports = {
   someSidebar: {
-    'Swarm for Enterprise': ['toc', 'problems', 'intro', 'swarm',],
-    'Data economy':['data-economy', 'catalyst', 'ethereum', 'new-paradigm', 'data-spaces', 'recommendations', ],
-    Privacy: ['privacy', 'swarm-privacy', ],
-    'Regulation': ['gdpr', 'consents',],
+    // 'Swarm for Enterprise': ['toc', 'problems', 'intro', 'swarm',],
+    // 'Data economy':['data-economy', 'catalyst', 'ethereum', 'new-paradigm', 'data-spaces', 'recommendations', ],
+    // Privacy: ['privacy', 'swarm-privacy', ],
+    // 'Regulation': ['gdpr', 'consents',],
     Examples: ['fairos', 'fairdrive', 'personium', 'fairdrop',],
     References: ['references',],
     Contribution: ['acknowledgements', 'contribute',],
   },
-
+  themeConfig: {
+  sidebarCollapsible: false,
+  // ...
+},
 };
